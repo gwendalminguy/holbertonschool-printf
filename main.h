6a70ba3aef4f00d0480df8e5da794588aeefb53b
smessaoui31 <messaoui.sofian@hotmail.fr git config --global user.email messaoui.sofian@hotmail.fr>
2025-03-24 10:42:31 +0000

    Creation of the main header file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <starg.h>
+#include <stlib.h>
+#include <unistd.h>
+
+/**
+ * struct conv : Define a structure of functions and arguments identifier
+ * @id : identifier for a type
+ * @*fptr : A function pointer to a function
+ */
+
+
+struct conv
+{
+	char id; 
+	void (*fptr)(va_list); 
+};
+
+typedef struct conv conv_t;
+
+/* All our functions*/
+void print_string(va_list);
+void print_char(va_list);
+
+#endif
