a84f1a6bcf8df2cd85e1f2080240ed0d11dcf5f1
smessaoui31 <messaoui.sofian@hotmail.fr git config --global user.email messaoui.sofian@hotmail.fr>
2025-03-24 14:03:56 +0000

    Change the argument and fixing betty style

--- print_string.c
+++ print_string.c
@@ -4,13 +4,13 @@
 int print_string(va_list *args);
 {
 
-	int i; 
+	int i;
 	char *string;
 
-	string = (va_args(list char *));
-	if (string == 0);
-	string = "(null)";
+	string = (va_arg(*args, char *));
+	if (string == 0)
+		string = "(null)";
 	for (i = 0; string[i] != '\0'; i++)
 		_putchar(string[i]);
-	return(i);
+	return (i);
 }
