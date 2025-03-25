c9f998c6c0b02ab20e461a2a0efde075c1144faf
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-25 13:09:41 +0000

    Fixing the print_int file and completed it + added it in the header file

--- main.h
+++ main.h
@@ -23,5 +23,6 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(va_list *args);
 int print_char(va_list *args);
+int print_int(va_list *args);
 
 #endif
---
+++ print_int.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+
+
+
+
+int print_int(va_list *args)
+{
+
+	int value;
+	unsigned int abs, a , len;
+	unsigned int count = 1;
+
+	len = 0;
+	value = va_args(*args , int);
+
+	if (value < 0)/* Manage negative numbers*/
+	{
+		len = len + _putchar('-');
+		abs = value * -1;
+	}
+	else 
+		abs = value;
+	
+	a = abs; /* Store the absolute value into the variable 'a' for extraction*/
+
+	while (a > 9)/*loop to calculate how many digit  abs contain*/
+	{
+		a = a / 10;
+		count = count * 10;
+	}
+
+	while(count >= 1)/* Extract and print each digit*/
+	{
+		len = len + _putchar(((abs/count) % 10) + '0')/* Print each digit of the number*/
+			count = count / 10; /* Move into the next digit*/
+	}
+	/* Return the total number of character printed*/
+	return (len);
+}
