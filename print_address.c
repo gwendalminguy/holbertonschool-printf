bc31193266211f8128ed1a8a204ebb7a7a89d6d3
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-27 16:52:46 +0100

    Created function print_address

---
+++ print_address.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_address - prints the address of any given variable
+ * @args: variable to print the address of
+ *
+ * Return: number of characters printed
+ */
+int print_address(va_list *args)
+{
+	int *ptr;
+	int len = 0;
+
+	/* Assigning the variable ptr to the address of the argument */
+	ptr = va_arg(*args, void *);
+
+	/* Getting number of digits of ptr in hexadecimal */ 
+	while (raise(16, len) <= (long int)ptr)
+		len++;
+
+	_putchar('0');
+	_putchar('x');
+	len += 2;
+
+	/* Printing ptr as an hexadecimal value */
+	_printf("%x", ptr);
+
+	return (len);
+}
