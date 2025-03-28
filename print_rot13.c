e978c1e9277759a09ae17397a9920c68d5cde2ef
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-28 16:33:21 +0100

    Corrected with betty coding style

--- print_rot13.c
+++ print_rot13.c
@@ -9,27 +9,27 @@
 int print_rot13(va_list *args)
 {
 	int i;
-	char *string;
+	char *str;
 
 	/* Getting the string argument from the va_list */
-	string = (va_arg(*args, char *));
+	str = (va_arg(*args, char *));
 
 	/* Checking if the string is NULL */
-	if (string == 0)
-		string = "(null)";
+	if (str == 0)
+		str = "(null)";
 
 	/* Looping through each character of the string */
-	for (i = 0; string[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		/* Shifting by adding 13 if in first half of alphabet */
-		if ((string[i] > 64 && string[i] < 78) || (string[i] > 96 && string[i] < 110))
-			_putchar(string[i] + 13);
+		if ((str[i] > 64 && str[i] < 78) || (str[i] > 96 && str[i] < 110))
+			_putchar(str[i] + 13);
 		/* Shifting by removing 13 if in second half of alphabet */
-		else if ((string[i] > 77 && string[i] < 91) || (string[i] > 109 && string[i] < 123))
-			_putchar(string[i] - 13);
+		else if ((str[i] > 77 && str[i] < 91) || (str[i] > 109 && str[i] < 123))
+			_putchar(str[i] - 13);
 		/* Printing as is if not an alphabet character */
 		else
-			_putchar(string[i]);
+			_putchar(str[i]);
 	}
 
 	return (i);
